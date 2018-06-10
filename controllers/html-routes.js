5b1f86fbec4321f0840b53864ffc9292d035dcb9
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-06-10 16:04:13 -0700

    Html routes updated

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,5 +1,9 @@
 module.exports = function(app){
 
+    app.get("/", function(req,res){
+        res.render("home");
+    })
+
     app.get("/list-items", function(req,res){
         res.render("query");
     });
@@ -8,25 +12,28 @@ module.exports = function(app){
         res.render("accounts");
     });
 
-    app.get("/account/:account_id/:account_key" function(req,res){
+    app.get("/add-items", function(req, res){
         res.render("add-items");
-    });
+    })
+
+    // app.get("/account/:account_id/:account_key" function(req,res){
+    //     res.render("add-items");
+    // });
 
 
-    app.get("/list-items/items", function(req,res){
-        db.Items.findAll({
-        }).then(function(result){
-            res.render(result);
-        });
-    });
-
-    // app.put("/list-items/items/:account_id/:account_key", function(req,res){
+    // app.get("/list-items/items", function(req,res){
     //     db.Items.findAll({
     //     }).then(function(result){
     //         res.render(result);
     //     });
     // });
 
+    // app.put("/list-items/items/:account_id/:account_key", function(req,res){
+    //     db.Items.update({
 
+    //     }).then(function(result){
+    //         res.render(result);
+    //     });
+    // });
 
-}
+};
