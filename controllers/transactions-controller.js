4f91302d85b794c56e02730eedfc2c64e9a48042
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-06-11 10:50:19 -0700

    Transactions Controller Updated

--- controllers/transactions-controller.js
+++ controllers/transactions-controller.js
@@ -2,4 +2,38 @@ var db = require("../models");
 
 module.exports = function(app){
 
+	app.get("/api/transactions/:account_id/:account_key", function(req,res){
+        db.Transactions.findAll({
+            where: {
+                id: req.params.account_id,
+                account_key: req.params.account_key
+            }
+        }).then(function(dbTransactions){
+            res.json(dbTransactions);
+        });
+    });
+
+    app.post("/api/transactions", function(req,res){
+        db.Transacations.create({
+            start_date: req.body.start_date,
+            end_date: req.body.end_date,
+            total_cost: req.body.total_cost,
+            renter_id: req.body.renter_id,
+            item_id: req.body.item_id
+        }).then(function(dbTransactions){
+            res.json(dbTransactions);
+        });
+    });
+
+    app.delete("/api/transactions/:account_id/:account_key", function(req,res){
+        db.Transactions.destroy({
+            where: {
+                id: req.params.account_id,
+                account_key: req.params.account_key
+            }
+        }).then(function(dnTransactions){
+            res.json(dbTransactions);
+        })
+    })
+
 };
