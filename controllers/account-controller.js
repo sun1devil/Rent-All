4f4a2d7f5edc200c4406f0089455082d2f84bb9a
Okens <ng.eric314@gmail.com>
2018-06-11 15:22:39 -0700

    Updating account controller.

--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -1,19 +1,22 @@
 var db = require("../models");
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    app.get("/api/accounts/:account_id/:account_key", function(req,res){
-        db.Accounts.findAll({
+    app.get("/accounts/view/:account_id/:account_key", function (req, res) {
+        db.Accounts.findOne({
             where: {
                 id: req.params.account_id,
                 account_key: req.params.account_key
             }
-        }).then(function(dbAccounts){
-            res.json(dbAccounts);
+        }).then(function (dbAccounts) {
+            var hbsObj = {
+                account: dbAccounts
+            }
+            res.render("view", hbsObj);
         });
     });
 
-    app.post("/api/accounts", function(req,res){
+    app.post("/accounts/new", function (req, res) {
         db.Accounts.create({
             first_name: req.body.first_name,
             last_name: req.body.last_name,
@@ -24,20 +27,42 @@ module.exports = function(app){
             balance: req.body.balance,
             email: req.body.email,
             account_key: req.body.account_key
-        }).then(function(dbAccounts){
+        }).then(function (dbAccounts) {
             res.json(dbAccounts);
         });
     });
 
-    app.delete("/api/accounts/:account_id/:account_key", function(req,res){
+    app.delete("/accounts/:account_id/:account_key", function (req, res) {
         db.Accounts.destroy({
             where: {
                 id: req.params.account_id,
                 account_key: req.params.account_key
             }
-        }).then(function(dbAccounts){
+        }).then(function (dbAccounts) {
             res.json(dbAccounts);
         })
-    })
+    });
+
+
+    app.put("/accounts/:account_id/:account_key", function (req, res) {
+        db.Accounts.update({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            street: req.body.street,
+            city: req.body.city,
+            state: req.body.state,
+            zip: req.body.zip,
+            balance: req.body.balance,
+            email: req.body.email,
+            account_key: req.body.account_key
+        }, {
+                where: {
+                    id: req.params.account_id,
+                    account_key: req.params.account_key
+                }
+            }).then(function (dbAccounts) {
+                res.json(dbAccounts);
+            })
+    });
 
 }
