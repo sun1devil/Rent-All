4f9b515abfcdabc589fd9d4a56e25247c2af4fd0
Linh Tong <linhthitong@gmail.com>
2018-06-12 10:21:00 -0700

    create home.js logic

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,4 +1,25 @@
 
 // on click of each button with AJAX call based on predefined routes - look into Google doc for routes
   
-  
+
+// when user clicks search button
+$("#search-item").on("click", function(event) {
+    event.preventDefault();
+
+    var listItems = {
+        items: $("#search-item").val().trim()
+    };
+
+ // Send the GET request.
+ $.ajax("/items", {
+    type: "GET",
+    data: listItems
+  }).then(
+    function() {
+      console.log("List of Items for Rent");
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  )
+});
+
