0b9f90db0525dbc077ee57faaa73405d41ba4b15
Linh Tong <linhthitong@gmail.com>
2018-06-12 10:44:09 -0700

    comment out AJAX on home.js

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -6,20 +6,20 @@
 $("#search-item").on("click", function(event) {
     event.preventDefault();
 
-    var listItems = {
-        items: $("#search-item").val().trim()
-    };
+    // var listItems = {
+    //     items: $("#search-item").val().trim()
+    // };
 
  // Send the GET request.
- $.ajax("/items", {
-    type: "GET",
-    data: listItems
-  }).then(
-    function() {
-      console.log("List of Items for Rent");
+//  $.ajax("/items", {
+    // type: "GET"
+    // data: listItems
+  // }).then(
+    // function() {
+      // console.log("List of Items for Rent");
       // Reload the page to get the updated list
-      location.reload();
+      // location.reload();
     }
   )
-});
+// });
 
