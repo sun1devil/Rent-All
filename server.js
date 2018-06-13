ed097e9ff678de7d269b8ed40c2a7bfc7c5e8510
Okens <ng.eric314@gmail.com>
2018-06-13 15:30:19 -0700

    Updating server.js

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine","handlebars");
 
