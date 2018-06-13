1928d7bd383e40461a51b597b5023e97046e7131
Okens <ng.eric314@gmail.com>
2018-06-13 15:15:34 -0700

    Updating config.js with heroku variable.

--- config/config.js
+++ config/config.js
@@ -19,10 +19,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": "password",
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 }
