f699af04c41ac0edfa0dff8f61bb438174f4cd3e
Okens <ng.eric314@gmail.com>
2018-06-12 18:37:06 -0700

    Adding functionality to home page to redirect.

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,25 +1,31 @@
 
 // on click of each button with AJAX call based on predefined routes - look into Google doc for routes
-  
+
 
 // when user clicks search button
-$("#search-item").on("click", function(event) {
-    event.preventDefault();
+$("#search-item").on("click", function (event) {
+  event.preventDefault();
 
-    // var listItems = {
-    //     items: $("#search-item").val().trim()
-    // };
+  console.log("Entering search")
 
- // Send the GET request.
-//  $.ajax("/items", {
-    // type: "GET"
-    // data: listItems
-  // }).then(
-    // function() {
-      // console.log("List of Items for Rent");
-      // Reload the page to get the updated list
-      // location.reload();
-    }
-  )
-// });
+  window.location.href = "/search/"
 
+});
+
+
+$("#post-item").on("click", function (event) {
+  event.preventDefault();
+
+  console.log("Entering search")
+
+  window.location.href = "/post-items/new"
+  
+});
+$("#sign-up").on("click", function (event) {
+  event.preventDefault();
+
+  console.log("Entering search")
+
+  window.location.href = "/accounts/new"
+  
+});
