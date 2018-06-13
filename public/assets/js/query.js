f61ae5e06ed9fd4d0be248594663e2f4751ebfb3
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-06-13 14:35:07 -0700

    Rent Styling

--- public/assets/js/query.js
+++ public/assets/js/query.js
@@ -1,24 +1,35 @@
 console.log("Query.js loaded");
 
-    $(".change-rent").on("click", function(event) {
-        event.preventDefault();
+  // $("#rentModal").modal("hide");
 
-      var id = $(this).data("id");
-      var newRent = $(this).data("newrent");
+      $("#select-rent").on("click", function(event) {
+          event.preventDefault();
+          $("#rentModal").modal("show");
+      });
 
-      var newRentState = {
-        availability: newRent
-      };
 
-      // Send the PUT request.
-      $.ajax("/post-items/update" + id, {
-        type: "PUT",
-        data: newRentState
-      }).then(
-        function() {
-          console.log("changed availability to: ", newRent);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+      $("#submit-rent").on("click", function(event){
+          event.preventDefault();
+          var id = $(this).data("id");
+          var newRent = $(this).data("newrent");
+    
+          var newRentState = {
+            availability: newRent
+          };
+
+            // Send the PUT request.
+        $.ajax("/post-items/update" + id, {
+          type: "PUT",
+          data: newRentState
+        }).then(
+          function() {
+            console.log("changed availability to: ", newRent);
+            // $("#rentModal").modal("hide");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+
+      });
+
+   
