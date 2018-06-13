8b4ccea3377058e074466ea9919c4ba2c59fcedd
Okens <ng.eric314@gmail.com>
2018-06-13 11:42:06 -0700

    Adding firebase requirements and image loading for items.

--- public/assets/js/firebase_config.js
+++ public/assets/js/firebase_config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 
 }
