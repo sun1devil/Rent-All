0fe089ef1491cfd60892158e8453a003304347d2
Okens <ng.eric314@gmail.com>
2018-06-13 17:21:05 -0700

    Updating error on id in query-block for submitting. Changing id to class.

--- public/assets/js/query.js
+++ public/assets/js/query.js
@@ -2,7 +2,7 @@ console.log("Query.js loaded");
 
 // $("#rentModal").modal("hide");
 
-$("#rentThisItem").on("click", function (event) {
+$(".rentThisItem").on("click", function (event) {
   event.preventDefault();
   $("#rentModal").modal("show");
 });
